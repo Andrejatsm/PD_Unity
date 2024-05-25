9b87dca6fafaf9fdf7a5a30d1affd9d1ebc1d0d2
Andrejs Račkovs <piradzins8@gmail.com>
2024-05-25 19:13:40 +0300

    Final version(broken)

---
+++ Assets/Script/ShowTextScripts.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowTextScrip : MonoBehaviour
+{
+    string text;
+    int num;
+    public GameObject inputNameField;
+    public GameObject inputAgeField;
+    public GameObject displayField;
+    string[] words = { "Labrit ", "Labdien ", "Sveiks ","Food " };
+    int randomNumb;
+
+
+   public void GetText()
+    {
+        randomNumb = Random.Range(0, words.Length);
+        text = inputNameField.GetComponent<InputField>().text;
+        num = int.Parse(inputAgeField.GetComponent<InputField>().text);
+        int currentYear = System.DateTime.Now.Year;
+        displayField.GetComponent<Text>().text =
+            words[randomNumb] + text.ToLower() + "! You're " + (currentYear - num) + " years old";
+    }
+}
+
